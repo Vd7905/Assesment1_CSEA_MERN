d20b8c961f1e86c23c66406568db73453e1a6598
Vd7905 <vikasdixit7905@gmail.com>
2024-11-08 15:18:58 +0530

    resume\

---
+++ index.js
@@ -0,0 +1,6 @@
+const generate = document.getElementById('generate')
+const wrapper = document.getElementsByClassName('wrapper')
+
+function GenerateResume(){
+    wrapper.style.visibility = "hidden";
+}
